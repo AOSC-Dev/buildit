ec045192aaf0516c69ab162e5a31e5fb3bc6d103
eatradish <sakiiily@aosc.io>
2023-12-21 14:53:15 +0800

    chore(script): add restart Yerus buildit-mips64r6el

--- scripts/restart-all-worker.sh
+++ scripts/restart-all-worker.sh
@@ -3,3 +3,5 @@
 for i in Yerus-cn Resonance-cn Catfish-cn Zinfandel-cn Mio-cn kp920-cn PowerNV-cn GreenGoo-cn Stomatopoda-cn PorterAlePro-cn; do 
     echo "$i" && ssh root@$i "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service";
 done
+
+ssh root@Yerus-cn "cd /buildroots/buildit-mips64r6el/buildit && git pull && systemctl restart buildit-worker.service"
