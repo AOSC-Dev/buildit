b6afa153036b36a1cf5759a2af958b0f24992090
eatradish <sakiiily@aosc.io>
2023-12-26 18:46:11 +0800

    script: fill of all buildbot

--- scripts/restart-all-worker.sh
+++ scripts/restart-all-worker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-for i in Yerus-cn Resonance-cn Catfish-cn Zinfandel-cn Mio-cn kp920-cn PowerNV-cn GreenGoo-cn Stomatopoda-cn PorterAlePro-cn; do 
+for i in Yerus-cn Resonance-cn Catfish-cn Mio-cn kp920-cn eleventh PowerNV-cn GreenGoo-cn Stomatopoda-cn PorterAlePro-cn Taple-cn; do 
     echo "$i" && ssh root@$i "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service";
 done
-
-ssh root@Yerus-cn "systemctl restart buildit-worker-mips64r6el.service"
