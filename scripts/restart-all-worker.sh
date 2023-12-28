f1e2dea590dc8902c3067086796d5f167ca99bce
Mingcong Bai <jeffbai@aosc.xyz>
2023-12-27 20:22:44 -0800

    scripts: use dynamic server list; improve output

--- scripts/restart-all-worker.sh
+++ scripts/restart-all-worker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for i in Yerus-cn Resonance-cn Catfish-cn Mio-cn kp920-cn eleventh PowerNV-cn GreenGoo-cn Stomatopoda-cn PorterAlePro-cn Taple-cn; do 
-    echo "$i" && ssh root@$i "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service";
+for i in `cat servers.list`; do
+    unset server_name server_port
+    server_name=`awk '{ print $1 }' servers.list`
+    server_port=`awk '{ print $2 }' servers.list`
+    printf "Restarting BuildIt worker on $server_name ...    "
+    ssh root@relay.aosc.io -p $i \
+        "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service" \
+            && printf "OK!\n" \
+            || printf "Failed!\n"
+    unset server_name server_port
 done
