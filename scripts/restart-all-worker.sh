4d8b6babe7e303c0c01f7b1f4d1750ef849f6c85
Mingcong Bai <jeffbai@aosc.xyz>
2023-12-28 22:22:20 -0800

    fix: fix worker restart script

--- scripts/restart-all-worker.sh
+++ scripts/restart-all-worker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in `cat servers.list`; do
+while read i; do
     unset server_name server_port
-    server_name=`awk '{ print $1 }' servers.list`
-    server_port=`awk '{ print $2 }' servers.list`
-    printf "Restarting BuildIt worker on $server_name ...    "
-    ssh root@relay.aosc.io -p $i \
+    server_name=$(echo $i | awk '{ print $1 }')
+    server_port=$(echo $i | awk '{ print $2 }')
+    printf "Restarting BuildIt worker on $server_name ($server_port) ... "
+    ssh root@relay.aosc.io -p $server_port \
         "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service" \
             && printf "OK!\n" \
             || printf "Failed!\n"
     unset server_name server_port
-done
+done < servers.list
