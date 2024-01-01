bb9d8fdc7ef90fdd18998ccc8712850665d177c6
Mingcong Bai <jeffbai@aosc.xyz>
2023-12-31 20:38:29 -0800

    fix: (restart-all-worker.sh) fix while read loop

--- scripts/restart-all-worker.sh
+++ scripts/restart-all-worker.sh
@@ -5,9 +5,10 @@ while read i; do
     server_name=$(echo $i | awk '{ print $1 }')
     server_port=$(echo $i | awk '{ print $2 }')
     printf "Restarting BuildIt worker on $server_name ($server_port) ... "
-    ssh root@relay.aosc.io -p $server_port \
-        "cd /buildroots/buildit/buildit && git pull && systemctl restart buildit-worker.service" \
+    # ssh -n disables reading from stdin, which overrides the while read loop.
+    ssh -n root@relay.aosc.io -p $server_port \
+        "cd /buildroots/buildit/buildit && git pull -q && systemctl restart buildit-worker.service" \
             && printf "OK!\n" \
-            || printf "Failed!\n"
+            || printf "Failed to restart BuildIt worker on $server_name ($server_port)!\n"
     unset server_name server_port
 done < servers.list
