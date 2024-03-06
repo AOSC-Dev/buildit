ad51e39f87f27350b1fb870603456ade01a74523
Jiajie Chen <c@jia.je>
2024-03-07 00:50:00 +0800

    feat: do not reuse connection in worker, since we have our own rabbitmq server without connection limit

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,44 +1,8 @@
 use clap::Parser;
-use lapin::{Channel, Connection, ConnectionProperties};
-use once_cell::sync::Lazy;
-use std::{path::PathBuf, sync::Arc};
-use tokio::sync::Mutex;
+use std::path::PathBuf;
 
 pub mod build;
 pub mod heartbeat;
-
-pub static CONNECTION: Lazy<Arc<Mutex<Option<Connection>>>> =
-    Lazy::new(|| Arc::new(Mutex::new(None)));
-
-// try to reuse amqp channel
-pub async fn ensure_channel(args: &Args) -> anyhow::Result<Channel> {
-    let mut lock = CONNECTION.lock().await;
-    let conn = match &*lock {
-        Some(conn) => {
-            if conn.status().connected() {
-                conn
-            } else {
-                // re-connect
-                *lock = None;
-
-                let conn =
-                    lapin::Connection::connect(&args.amqp_addr, ConnectionProperties::default())
-                        .await?;
-                *lock = Some(conn);
-                lock.as_ref().unwrap()
-            }
-        }
-        None => {
-            let conn = lapin::Connection::connect(&args.amqp_addr, ConnectionProperties::default())
-                .await?;
-            *lock = Some(conn);
-            lock.as_ref().unwrap()
-        }
-    };
-
-    let channel = conn.create_channel().await?;
-    Ok(channel)
-}
 
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
