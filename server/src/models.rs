8aaaf45043d6f8c6e35e7be0636650669e9fea3e
Jiajie Chen <c@jia.je>
2024-03-10 11:30:02 +0800

    feat: create jobs and workers table

--- server/src/models.rs
+++ server/src/models.rs
@@ -11,3 +11,28 @@ pub struct Pipeline {
     pub git_sha: String,
     pub creation_time: chrono::DateTime<chrono::Utc>,
 }
+
+#[derive(Queryable, Selectable, Associations)]
+#[diesel(belongs_to(Pipeline))]
+#[diesel(table_name = crate::schema::jobs)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Job {
+    pub id: i32,
+    pub pipeline_id: i32,
+    pub packages: String,
+    pub arch: String,
+    pub creation_time: chrono::DateTime<chrono::Utc>,
+    pub status: String,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::workers)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Worker {
+    pub id: i32,
+    pub hostname: String,
+    pub arch: String,
+    pub git_commit: String,
+    pub memory_bytes: i64,
+    pub logical_cores: i32,
+}
--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    jobs (id) {
+        id -> Int4,
+        pipeline_id -> Int4,
+        packages -> Text,
+        arch -> Text,
+        creation_time -> Timestamptz,
+        status -> Text,
+    }
+}
+
+diesel::table! {
     pipelines (id) {
         id -> Int4,
         packages -> Text,
@@ -10,3 +21,18 @@ diesel::table! {
         creation_time -> Timestamptz,
     }
 }
+
+diesel::table! {
+    workers (id) {
+        id -> Int4,
+        hostname -> Text,
+        arch -> Text,
+        git_commit -> Text,
+        memory_bytes -> Int8,
+        logical_cores -> Int4,
+    }
+}
+
+diesel::joinable!(jobs -> pipelines (pipeline_id));
+
+diesel::allow_tables_to_appear_in_same_query!(jobs, pipelines, workers,);
