f8c27bf37848c74e09cb4f91f408c4930940edcf
Jiajie Chen <c@jia.je>
2024-03-11 20:11:46 +0800

    feat: add initial frontend layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app>
-    <v-main>
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text="Login" variant="text"></v-btn>
+      </v-container>
+    </v-app-bar>
+    <v-main class="bg-grey-lighten-3">
       <router-view />
     </v-main>
   </v-app>
@@ -8,4 +20,17 @@
 
 <script lang="ts" setup>
   //
+</script>
+
+<script lang="ts">
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Pipelines',
+        'Jobs',
+        'Workers',
+      ],
+    }),
+  }
 </script>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card height="200" title="Pipelines"></v-card>
+      </v-col>
+      <v-col>
+        <v-card height="200" title="Jobs"></v-card>
+      </v-col>
+      <v-col>
+        <v-card height="200" title="Workers"></v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
   //
+</script>
+
+<script lang="ts">
+  import axios from 'axios';
+  export default {
+    data: () => ({
+      items: [
+        'Workers',
+        'Pipelines',
+        'Jobs',
+      ],
+    }),
+  }
 </script>
