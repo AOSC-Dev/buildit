812a17fca8a985055da583d70fdcf9ef032fbff1
Jiajie Chen <c@jia.je>
2024-03-13 11:36:21 +0800

    feat: add web-logs for pretty logs

---
+++ frontend/src/pages/web-logs/[name].vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container style="height: 100%;">
+    <div ref="element" style="height: 100%;">
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
+
+<script lang="ts">
+  import axios from 'axios';
+  import { hostname } from '@/common';
+  import '@xterm/xterm/css/xterm.css';
+  import { Terminal } from '@xterm/xterm';
+  import { FitAddon } from '@xterm/addon-fit';
+
+  export default {
+    mounted() {
+      this.fetchData();
+    },
+    data: () => ({
+    }),
+    methods: {
+      async fetchData() {
+        let name = (this.$route.params as { name: string }).name;
+        let term = new Terminal({ convertEol: true });
+        let fitAddon = new FitAddon();
+        term.loadAddon(fitAddon);
+        term.open(this.$refs.element, true);
+        fitAddon.fit();
+        term.write((await axios.get(`/logs/${name}`)).data);
+      }
+    }
+  }
+</script>
