00e288b61db54b8201dd40a6f088e1952c8ff0f5
eatradish <sakiiily@aosc.io>
2024-06-19 14:30:31 +0800

    feat(frontend): add buildit-monitor page

---
+++ frontend/src/pages/monitor/[hostname].vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container style="height: 100%; font-family: monospace">
+    <div v-html="html"></div>
+  </v-container>
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    this.fetchData();
+  },
+  data: () => ({
+    html: "",
+    lines: 0,
+  }),
+  methods: {
+    fetchData() {
+      let name = (this.$route.params as { hostname: string }).hostname;
+      const socket = new WebSocket(
+        `wss://buildit.aosc.io/api/ws/viewer/${name}`
+      );
+      socket.onmessage = (event) => {
+        if (this.lines > 5000) {
+          this.html = "";
+          this.lines = 0;
+        }
+        const data = event.data;
+        this.html += data + " <br/> ";
+        this.line += 1;
+        window.scrollTo(0, document.body.scrollHeight);
+      };
+    },
+  },
+};
+</script>
