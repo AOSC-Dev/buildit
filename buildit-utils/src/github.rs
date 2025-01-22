AOSC-Dev/buildit

commit f2d87dbb1ddba3e291d8d60edd00e34be47d7698

fix: escape commit messages in openpr

buildit-utils/src/github.rs
@@ -235,13 +235,13 @@ fn handle_commits(commits: &[Commit]) -> anyhow::Result<String> {
             break;
         }
 
-        s.push_str(&format!("- {}\n", c.msg.0.trim()));
+        s.push_str(&format!("- {}\n", escape(c.msg.0.trim())));
         if let Some(body) = &c.msg.1 {
             let body = body.split('\n');
             for line in body {
                 let line = line.trim();
                 if !line.is_empty() {
-                    s.push_str(&format!("    {line}\n"));
+                    s.push_str(&format!("    {}\n", escape(line.trim())));
                 }
             }
         }
@@ -252,6 +252,22 @@ fn handle_commits(commits: &[Commit]) -> anyhow::Result<String> {
     }
 
     Ok(s)
+}
+
+fn escape(text: &str) -> String {
+    // the escaped strings only live for a short time, and they are short
+    // so the waste of memeory are ignorable
+    let mut result = String::with_capacity(text.len() * 2);
+    for char in text.chars() {
+        if matches!(
+            char,
+            '*' | '~' | '`' | '[' | ']' | '(' | ')' | '_' | '-' | '\\'
+        ) {
+            result.push('\\');
+        }
+        result.push(char);
+    }
+    result
 }
 
 struct Commit {

commit d03fae643050f95cd93db1b23532de995950fb8f

fix: keep '-' and '_' unescaped in PRs
The Markdown parser of GitHub seems to follow the CommonMark
specification (partially). Per spec 0.31.2, example 20, escape
cannot be used in autolinks. If we have a '-' escaped as '\-' in
a URL in our commit message, it will not be recognized as a escape
sequence.

What is out of CommonMark is that GitHub also recognize plain links
('https://xxx'), which are not even CommonMark autolinks ('<https://xxx>'),
as autolinks.

These two characters are common in URLs, they have to be used twice
('--') to have a styling semantic in Markdown texts, in the common case
they are used only once when used in package versions. Based on these
three reasons, I suggest removing them from the list.

Other escaped characters are also affected by the parser behavior, but
they are more common in our commit messages, less common in URLs, and
characters like '~' and '*' will have styling semantic even when there
is only a single symbol. Based on these, they are not removed.

Reported-by: Mingcong Bai <jeffbai@aosc.io>
Fixes: f2d87dbb1ddb ("fix: escape commit messages in openpr")
Link: https://spec.commonmark.org/0.31.2/#backslash-escapes
Signed-off-by: xtex <xtexchooser@duck.com>

buildit-utils/src/github.rs
@@ -261,7 +261,7 @@ fn escape(text: &str) -> String {
     for char in text.chars() {
         if matches!(
             char,
-            '*' | '~' | '`' | '[' | ']' | '(' | ')' | '_' | '-' | '\\'
+            '*' | '~' | '`' | '[' | ']' | '(' | ')' | '\\'
         ) {
             result.push('\\');
         }