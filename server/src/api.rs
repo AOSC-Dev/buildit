AOSC-Dev/buildit

commit 6aafb9f271db70f16dfea5e44da3d7ee89a185e7

feat: save creator user id when applicable

server/src/api.rs
@@ -1,12 +1,14 @@
 use crate::{
     github::{get_crab_github_installation, get_packages_from_pr},
-    models::{NewJob, NewPipeline, Pipeline, Worker},
+    models::{NewJob, NewPipeline, Pipeline, User, Worker},
     DbPool, ABBS_REPO_LOCK, ALL_ARCH, ARGS,
 };
 use anyhow::anyhow;
 use anyhow::Context;
 use buildit_utils::github::{get_archs, update_abbs};
-use diesel::{dsl::count, ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper};
+use diesel::{
+    dsl::count, ExpressionMethods, OptionalExtension, QueryDsl, RunQueryDsl, SelectableHelper,
+};
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use tracing::warn;
@@ -100,10 +102,18 @@ pub async fn pipeline_new(
         .get()
         .context("Failed to get db connection from pool")?;
     use crate::schema::pipelines;
-    let (source, github_pr, telegram_user) = match source {
-        JobSource::Telegram(id) => ("telegram", github_pr, Some(id)),
-        JobSource::Github(id) => ("github", Some(*id), None),
-        JobSource::Manual => ("manual", github_pr, None),
+    let (source, github_pr, telegram_user, creator_user_id) = match source {
+        JobSource::Telegram(id) => {
+            // lookup user id via telegram chat id
+            let user = crate::schema::users::dsl::users
+                .filter(crate::schema::users::dsl::telegram_chat_id.eq(id))
+                .first::<User>(&mut conn)
+                .optional()?;
+            let creator_user_id = user.map(|user| user.id);
+            ("telegram", github_pr, Some(id), creator_user_id)
+        }
+        JobSource::Github(id) => ("github", Some(*id), None, None),
+        JobSource::Manual => ("manual", github_pr, None, None),
     };
     let new_pipeline = NewPipeline {
         packages: packages.to_string(),
@@ -114,6 +124,7 @@ pub async fn pipeline_new(
         source: source.to_string(),
         github_pr: github_pr.map(|pr| pr as i64),
         telegram_user: telegram_user.copied(),
+        creator_user_id: creator_user_id,
     };
     let pipeline = diesel::insert_into(pipelines::table)
         .values(&new_pipeline)

server/migrations/2024-03-16-091643_add_creator_user_id_to_pipelines/down.sql
@@ -1,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+ALTER TABLE pipelines DROP CONSTRAINT creator_user;
+ALTER TABLE pipelines DROP COLUMN creator_user_id;

server/migrations/2024-03-16-091643_add_creator_user_id_to_pipelines/up.sql
@@ -1,0 +1,3 @@
+-- Your SQL goes here
+ALTER TABLE pipelines ADD COLUMN creator_user_id INT;
+ALTER TABLE pipelines ADD CONSTRAINT creator_user FOREIGN KEY(creator_user_id) REFERENCES users(id);

server/src/models.rs
@@ -14,6 +14,7 @@ pub struct Pipeline {
     pub source: String,
     pub github_pr: Option<i64>,
     pub telegram_user: Option<i64>,
+    pub creator_user_id: Option<i32>,
 }
 
 #[derive(Insertable)]
@@ -28,6 +29,7 @@ pub struct NewPipeline {
     pub source: String,
     pub github_pr: Option<i64>,
     pub telegram_user: Option<i64>,
+    pub creator_user_id: Option<i32>,
 }
 
 #[derive(Queryable, Selectable, Associations, Identifiable, Debug)]

server/src/schema.rs
@@ -34,6 +34,7 @@ diesel::table! {
         source -> Text,
         github_pr -> Nullable<Int8>,
         telegram_user -> Nullable<Int8>,
+        creator_user_id -> Nullable<Int4>,
     }
 }
 
@@ -62,5 +63,6 @@ diesel::table! {
 }
 
 diesel::joinable!(jobs -> pipelines (pipeline_id));
+diesel::joinable!(pipelines -> users (creator_user_id));
 
 diesel::allow_tables_to_appear_in_same_query!(jobs, pipelines, users, workers,);

commit 90de17d9b7bff0ae1e6fe657c31eee9721a2452d

fix(server): reject pipelines from unknown TG user
Fixes: 6aafb9f271db ("feat: save creator user id when applicable")
Signed-off-by: xtex <xtex@astrafall.org>

server/src/api.rs
@@ -155,10 +155,11 @@ pub async fn pipeline_new(
                 .filter(crate::schema::users::dsl::telegram_chat_id.eq(id))
                 .first::<User>(&mut conn)
                 .optional()?;
-            let creator_user_id = user.map(|user| user.id);
-            ("telegram", github_pr, Some(id), creator_user_id)
+            let creator_user_id = user.ok_or_else(|| anyhow!("requires login"))?.id;
+            ("telegram", github_pr, Some(id), Some(creator_user_id))
         }
         JobSource::GitHub { pr, user } => {
+            // creator membership is checked in webhook handler
             let user = crate::schema::users::dsl::users
                 .filter(crate::schema::users::dsl::github_id.eq(user))
                 .first::<User>(&mut conn)